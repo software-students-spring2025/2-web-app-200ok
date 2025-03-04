15ed89c742b97d4e50592be8bd5a341730d26db9
FrankFangH <hf2371@nyu.edu>
2025-03-03 20:07:22 -0500

    config update

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Force to load the data in .env
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path=dotenv_path, override=True)
 
 DSN = os.getenv("DSN", "mongodb://localhost/new")
 PORT = int(os.getenv("PORT", 3000))
